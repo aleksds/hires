8b624bed6e7cbb60a2588be5b5649d8b45ccc72b
Aleksandar Diamond-Stanic <adiamond@bates.edu>
2016-09-30 07:34:41 -0400

    version that read in galaxy and velocity information from an input file

--- hires_plot_mgii.py
+++ hires_plot_mgii.py
@@ -29,60 +29,25 @@ dir = os.environ['HIRESDIR']
 c = const.c.to('AA/s')
 
 # wavelengths of relevant absorption lines
-mgi2852 = 2852.96328 * u.AA
 mgii2803 = 2803.5314853 * u.AA
 mgii2796 = 2796.3542699 * u.AA
-feii2600 = 2600.1724835 * u.AA
-feii2586 = 2586.6495659 * u.AA
-feii2382 = 2382.7641781 * u.AA
-feii2374 = 2374.4603294 * u.AA
-feii2344 = 2344.2129601 * u.AA
 
-# galaxy names
-gal = ['J0826', 'J0901', 'J0905', 'J0944', 'J1125', 'J1219', 'J1232', 'J1341', 'J1450', 'J1506', 'J1558', 'J1613', 'J2116', 'J2140']
-
-# galaxy redshifts
-zem = np.array([0.603, 0.459, 0.712, 0.514, 0.519, 0.451, 0.400, 0.658, 0.782, 0.608, 0.402, 0.449, 0.728, 0.752])
-
-# approximate centroid velocities 
-vcen = np.array([-1250., -1330., -2570., -1240., -2130., -1950., -550., -390., -1790., -1110., -900., -2450., -1480., -770.])
-
-# approximate maximum velocities
-vmax = np.array([-1600., -1700., -3000., -2100., -2250., -2240., -750., -1600., -2100., -2090., -1400., -2650., -1800., -1150.])
+# arrays of galaxy names, redshifts, approximate centroid velocities,
+# approximate maximum velocities, and velocities to "flip" between the 2796
+# and 2803 line when constructing an empirical mgii velocity profile
+gal_info = ascii.read(dir+'gal_info.txt')
+gal = gal_info['gal']
+zem = gal_info['zem']
+vcen = gal_info['vcen']
+vmax = gal_info['vmax']
+vflip = gal_info['vflip'] * u.km / u.s
 
 # sort by maximum outflow velocity
 vmax_index = np.flipud(np.argsort(vmax))
 
 # define velocity ranges to plot the profile
-vra = np.zeros((len(gal), 3)) * u.km / u.s
-# J0826 
-vra[0] = [-3500., -1500., 1500.] * u.km / u.s
-# J0901 
-vra[1] = [-3500., -1700., 1500.] * u.km / u.s
-# J0905 
-vra[2] = [-3500., -3100., 1500.] * u.km / u.s
-# J0944 
-vra[3] = [-3500., -1400., 1500.] * u.km / u.s
-# J1125
-vra[4] = [-3500., -2300., 1500.] * u.km / u.s
-# J1219 
-vra[5] = [-3500., -2100., 1500.] * u.km / u.s
-# J1232 
-vra[6] = [-3500., -400., 1500.] * u.km / u.s
-# J1341 
-vra[7] = [-3500., -400., 1500.] * u.km / u.s
-# J1450 
-vra[8] = [-3500., -2200., 1500.] * u.km / u.s
-# J1506
-vra[9] = [-3500., -1600., 1500.] * u.km / u.s
- # J1558
-vra[10] = [-3500., -1200., 1500.] * u.km / u.s
-# J1613 
-vra[11] = [-3500., -2000., 1500.] * u.km / u.s
-# J2116 
-vra[12] = [-3500., -1600., 1500.] * u.km / u.s
-# J2140 
-vra[13] = [-3500., -850., 1500.] * u.km / u.s
+vb = -3500. * u.km / u.s
+vr = 1500. * u.km / u.s
 
 # make plot showing one galaxy per page
 filename = 'all_mgii.pdf'
@@ -150,8 +115,10 @@ with PdfPages(filename) as pdf:
 
         # show the velocity profile using the 2796 line on the blue
         # side and the 2803 line on the red side
-        g2796 = (vel_mgii_2796 > vra[i,0]) & (vel_mgii_2796 < vra[i,1])
-        g2803 = (vel_mgii_2803 > vra[i,1]) & (vel_mgii_2803 < vra[i,2])
+        #g2796 = (vel_mgii_2796 > vra[i,0]) & (vel_mgii_2796 < vra[i,1])
+        g2796 = (vel_mgii_2796 > vb) & (vel_mgii_2796 < vflip[i])
+        #g2803 = (vel_mgii_2803 > vra[i,1]) & (vel_mgii_2803 < vra[i,2])
+        g2803 = (vel_mgii_2803 > vflip[i]) & (vel_mgii_2796 < vr)
         ax = fig.add_subplot(4,1,4)
         ax.set_xlim(xmin, xmax)
         ax.xaxis.set_minor_locator(minorLocator)
@@ -231,9 +198,10 @@ with PdfPages(filename) as pdf:
         vel_mgii_2796 = vel_mgii_2796_aa.to('km/s')
 
         # define the regions to use the 2796 profile and the regions to use the 2803 profile
-        g2796 = (vel_mgii_2796 > vra[indx,0]) & (vel_mgii_2796 < vra[indx,1])
-        g2803 = (vel_mgii_2803 > vra[indx,1]) & (vel_mgii_2803 < vra[indx,2])
-
+        #g2796 = (vel_mgii_2796 > vra[indx,0]) & (vel_mgii_2796 < vra[indx,1])
+        g2796 = (vel_mgii_2796 > vb) & (vel_mgii_2796 < vflip[indx])
+        #g2803 = (vel_mgii_2803 > vra[indx,1]) & (vel_mgii_2803 < vra[indx,2])
+        g2803 = (vel_mgii_2803 > vflip[indx]) & (vel_mgii_2803 < vr)
 
         # plot the profiles using the 2796 profile on the blue side
         # and the 2803 profile on the red side
